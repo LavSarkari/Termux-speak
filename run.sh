daf9070e806a87fce39c1559301e6b959f5f3278
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-09-03 10:12:13 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+pkg install termux-api
+echo " "
+pkg install figlet -y
+echo " "
+pkg install toilet -y
+echo " "                                                                                        pkg install cowsay -y
+echo " "
+gem install lolcat
+echo " "
+clear
+
+echo " "
+cowsay -f eyes LavSarkari| lolcat
+toilet -f big ' termux-speak' -F gay | lolcat
+echo -e "\e[35mPlease make sure you media volume is up.\e[0m";
+echo -e "\e[31mTo Exit Press Ctrl+C\e[0m";
+echo " "
+termux-tts-speak Hello, ... Dear, .... I am Termux Speak ... Created by LuvSarkari ..... Now you can type your text.
+echo Enter or past your Text👇
+echo " "
+termux-tts-speak
